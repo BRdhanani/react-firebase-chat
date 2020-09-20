c10b268320c072ade0dd2c2d4dc2c5fdc017e6fd
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-09-20 17:09:15 +0530

    signup screen added

--- src/pages/Signuppage/Signuppage.js
+++ src/pages/Signuppage/Signuppage.js
@@ -1,9 +1,88 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import { Form, Input, Button, Checkbox } from 'antd'
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+}
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+}
 
 const Signup = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
+  }
 
   return (
-    <h1>Signup</h1>
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Fullname"
+        name="Fullname"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your fullname!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Email"
+        name="Email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
 
